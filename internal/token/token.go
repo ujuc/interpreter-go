199ebbf24f4a5fe352717506291000809cd06342
Sungjin Kang <ujuc@ujuc.me>
2021-10-18 22:20:33 +0900

    feat: lexer, token 사용 법에대한 내용 추가
    - lexer test 를 이용하여 token까지 설정을 정의한다.

---
+++ internal/token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 식별자 + 리터럴
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// 연산자
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// 구분자
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// 예약어
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
