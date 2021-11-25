b35b75b1e19e2bfcc689aa3de618731e0982c0b5
Sungjin Kang <ujuc@ujuc.me>
2021-11-25 21:03:58 +0900

    feat: integer 리터럴 추가
    - object.Integer 메소드가 안먹힌다. 왜그런지는 차차 찾아봐야 할듯.

---
+++ internal/evaluator/evaluator.go
@@ -0,0 +1,32 @@
+package evaluator
+
+import (
+	"github.com/ujuc/monkey/internal/ast"
+	"github.com/ujuc/monkey/internal/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// 명령문
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// 표현식
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
